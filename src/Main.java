4445506318fd2c6af4a0b207d2c5a7a9b51c70c6
Diana Krapivina <mr.ikoma@mail.ru>
2021-03-14 20:51:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       int ticketPrice = 3000;
+       int priceOneMile = 20;
+       int numberOfMiles = ticketPrice/priceOneMile;
+        System.out.println("Количество начисленных мили за купленный билет = " + numberOfMiles);
+    }
+}
